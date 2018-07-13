fd27db5ba351f35392eadccf5e7cbfbc4c136175
dengziyang3 <2296904102@qq.com>
2018-07-13 16:40:39 +0800

    离线告警接口数据查询返回结果

--- Common-facestarepo/src/main/java/com/hzgc/common/facestarepo/table/alarm/StarepoServiceUtil.java
+++ Common-facestarepo/src/main/java/com/hzgc/common/facestarepo/table/alarm/StarepoServiceUtil.java
@@ -3,9 +3,7 @@ package com.hzgc.common.facestarepo.table.alarm;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-
 import com.hzgc.common.facestarepo.table.table.ObjectInfoTable;
-import lombok.Data;
 import org.apache.commons.lang.StringUtils;
 
 
@@ -63,7 +61,6 @@ public class StarepoServiceUtil {
         return objectInfoList;
     }
 
-    @Data
   public static class ObjectInfo {
         private String id;
         private String name;
@@ -78,5 +75,32 @@ public class StarepoServiceUtil {
         private Timestamp statustime;
         private Integer important;
         private Integer status;
-    }
+
+      public String getId() { return id; }
+      public void setId(String id) { this.id = id; }
+      public String getName() { return name; }
+      public void setName(String name) { this.name = name; }
+      public String getPkey() { return pkey; }
+      public void setPkey(String pkey) { this.pkey = pkey; }
+      public String getIdcard() { return idcard; }
+      public void setIdcard(String idcard) { this.idcard = idcard; }
+      public Integer getSex() { return sex; }
+      public void setSex(Integer sex) { this.sex = sex; }
+      public String getReason() { return reason; }
+      public void setReason(String reason) { this.reason = reason; }
+      public String getCreator() { return creator; }
+      public void setCreator(String creator) { this.creator = creator; }
+      public String getCphone() { return cphone; }
+      public void setCphone(String cphone) { this.cphone = cphone; }
+      public Timestamp getCreatetime() { return createtime; }
+      public void setCreatetime(Timestamp createtime) { this.createtime = createtime; }
+      public Timestamp getUpdatetime() { return updatetime; }
+      public void setUpdatetime(Timestamp updatetime) { this.updatetime = updatetime; }
+      public Timestamp getStatustime() { return statustime; }
+      public void setStatustime(Timestamp statustime) { this.statustime = statustime; }
+      public Integer getImportant() { return important; }
+      public void setImportant(Integer important) { this.important = important; }
+      public Integer getStatus() { return status; }
+      public void setStatus(Integer status) { this.status = status; }
+  }
 }
