7b56565ee542d8a4010cdd5b6478a4670caa71ad
ck0903 <ck@hzgosun.com>
2018-08-14 11:15:40 +0800

    人口管理模块

--- Common-faceclustering/src/main/java/com/hzgc/common/faceclustering/table/PersonRegionTable.java
+++ Common-faceclustering/src/main/java/com/hzgc/common/faceclustering/table/PersonRegionTable.java
@@ -2,7 +2,7 @@ package com.hzgc.common.faceclustering.table;
 
 public class PersonRegionTable {
     public static final String TABLE_NAME = "personregion";
-    public static final String REGION_ID = "id";               // 对象信息的唯一标志
-    public static final String REGION_NAME = "name";          //区域名
-    public static final String REGION_IPCIDS="ipcids";       //区域底下的设备名称
+    public static final byte[] COLUMNFAMILY = "region".getBytes();
+    public static final byte[] REGION_NAME = "regionName".getBytes();          //区域名
+    public static final byte[] REGION_IPCIDS="ipcids".getBytes();       //区域底下的设备名称
 }
