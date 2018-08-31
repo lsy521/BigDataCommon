5b1025b30d3d1f930f183df880a38815dbecd19c
hangde1314 <273967108@qq.com>
2018-08-31 18:03:22 +0800

    modify

---
+++ Common-carattribute/src/main/java/com/hzgc/common/carattribute/bean/CarLogistic.java
@@ -0,0 +1,15 @@
+package com.hzgc.common.carattribute.bean;
+
+import java.io.Serializable;
+
+/**
+ * 与其他属性的拼接运算
+ */
+public enum CarLogistic implements Serializable{
+
+/**
+ * 与其他属性的拼接运算
+ */
+    AND,
+    OR
+}
--- Common-jni/src/main/java/com/hzgc/jniface/PersonAttribute.java
+++ Common-jni/src/main/java/com/hzgc/jniface/PersonAttribute.java
@@ -1,4 +1,164 @@
 package com.hzgc.jniface;
 
 public class PersonAttribute {
+
+    private String age;
+
+    private String baby;
+
+    private String bag;
+
+    private String bottomColor;
+
+    private String bottomType;
+
+    private String hat;
+
+    private String hair;
+
+    private String knapSack;
+
+    private String MessengerBag;
+
+    private String orientation;
+
+    private String sex;
+
+    private String shoulderBag;
+
+    private String umbrella;
+
+    private String upperColor;
+
+    private String upperType;
+
+    private String carType;
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getBaby() {
+        return baby;
+    }
+
+    public void setBaby(String baby) {
+        this.baby = baby;
+    }
+
+    public String getBag() {
+        return bag;
+    }
+
+    public void setBag(String bag) {
+        this.bag = bag;
+    }
+
+    public String getBottomColor() {
+        return bottomColor;
+    }
+
+    public void setBottomColor(String bottomColor) {
+        this.bottomColor = bottomColor;
+    }
+
+    public String getBottomType() {
+        return bottomType;
+    }
+
+    public void setBottomType(String bottomType) {
+        this.bottomType = bottomType;
+    }
+
+    public String getHat() {
+        return hat;
+    }
+
+    public void setHat(String hat) {
+        this.hat = hat;
+    }
+
+    public String getKnapSack() {
+        return knapSack;
+    }
+
+    public void setKnapSack(String knapSack) {
+        this.knapSack = knapSack;
+    }
+
+    public String getMessengerBag() {
+        return MessengerBag;
+    }
+
+    public void setMessengerBag(String messengerBag) {
+        MessengerBag = messengerBag;
+    }
+
+    public String getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(String orientation) {
+        this.orientation = orientation;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getShoulderBag() {
+        return shoulderBag;
+    }
+
+    public void setShoulderBag(String shoulderBag) {
+        this.shoulderBag = shoulderBag;
+    }
+
+    public String getUmbrella() {
+        return umbrella;
+    }
+
+    public void setUmbrella(String umbrella) {
+        this.umbrella = umbrella;
+    }
+
+    public String getUpperColor() {
+        return upperColor;
+    }
+
+    public void setUpperColor(String upperColor) {
+        this.upperColor = upperColor;
+    }
+
+    public String getUpperType() {
+        return upperType;
+    }
+
+    public void setUpperType(String upperType) {
+        this.upperType = upperType;
+    }
+
+    public String getHair() {
+        return hair;
+    }
+
+    public void setHair(String hair) {
+        this.hair = hair;
+    }
+
+    public String getCarType() {
+        return carType;
+    }
+
+    public void setCarType(String carType) {
+        this.carType = carType;
+    }
 }
